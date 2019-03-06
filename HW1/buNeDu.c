4b1846469dc27bdba1f29a46d19d175bfdfe66d3
Halil Onur Çeçen <cecenonur0@gmail.com>
2019-03-06 22:40:23 +0300

    - renamed executable - added single file information controls - converted stat into lstat for link information.

--- HW1/buNeDu.c
+++ HW1/buNeDu.c
@@ -26,11 +26,13 @@ int main(int argc, char *argv[]){
         detailFlag = true;
         depthFirstApply(argv[2], sizePathFun);
     }
-    else if (argc > 2 && strcmp(argv[2], "-z") == 0) {
-        detailFlag = true;
+    else {
+        if (argc > 2 && strcmp(argv[2], "-z") == 0) {
+            detailFlag = true;
+        }
+        depthFirstApply(argv[1], sizePathFun);
     }
-
-    depthFirstApply(argv[1], sizePathFun);
+    
 
     return 0;
 }
@@ -41,41 +43,68 @@ int depthFirstApply(char * path, int pathfun(char * path1)){
     long currentPathSize, totalPathSize = 0, subDirSize;
     char * currentPath;
     const char seperator[2] = "/";
+    struct stat fileStat;
     d = opendir(path);
     if (d) {
-        dir = readdir(d);
-        dir = readdir(d);
         while((dir = readdir(d)) != NULL) {
+            /* Append file name to the directory */
+            if (strlen(dir->d_name) > 0 && dir->d_name[0] == '.') continue;
             currentPath = (char *) malloc(strlen(path) + strlen(dir->d_name) + 2);
             strcpy(currentPath, path);
             if (path[strlen(path) - 1] != '/'){
-                strcat(currentPath, seperator);
+                strcat(currentPath, seperator); /* Also add a slash if it doesnt have one in the end */
             }
             strcat(currentPath, dir->d_name);
             
-            
+            /* If its a directory enter into recursively. This has priority over pathfun */
             if (dir->d_type == DT_DIR) {
                 subDirSize = depthFirstApply(currentPath, pathfun);
                 if (detailFlag && subDirSize > 0) totalPathSize += subDirSize;
             }
+            /* retrieve file info */
             else {
                 currentPathSize = pathfun(currentPath);
                 if (currentPathSize > 0) {
                     totalPathSize += currentPathSize;
-                }   
+                }
             }
 
             free(currentPath);
-
         }
         closedir(d);
     }
     else {
-        printf("Couldn't open dir %s \n", path);
-        
+        /* Check if given path is a single file and if so open it and print its details. */
+        if ((currentPathSize = pathfun(path)) >= 0){
+            if (lstat(path, &fileStat) < 0){
+                printf("Couldn't opened given file.\n");
+                return -2;
+            }
+            printf("Information for %s\n",path);
+            printf("---------------------------\n");
+            printf("File Size: %ld bytes \tBlock Size: %ld \tBlocks: %ld\n",fileStat.st_size, fileStat.st_blksize, fileStat.st_blocks);
+            printf("Number of Links: \t%ld\n",fileStat.st_nlink);
+            printf("File inode: \t\t%ld\n",fileStat.st_ino);
+            printf("File mode: \t\t%d\n",fileStat.st_mode);
+            printf("File Permissions: \t");
+            printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+            printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+            printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+            printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+            printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+            printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+            printf("\n");
+        }
+        else if (currentPathSize != -1){
+            printf("Couldn't open directory/file %s \n", path);
+        }
         return -1;
     }
-    printf("%ld\t\t%s\n", totalPathSize, path);
+    printf("%ld\t%s\n", totalPathSize, path);
     
     return totalPathSize;
 }
@@ -83,32 +112,11 @@ int depthFirstApply(char * path, int pathfun(char * path1)){
 int sizePathFun(char * path){
     struct stat fileStat;
     /* Error checking */
-    if (stat(path, &fileStat) < 0) 
+    if (lstat(path, &fileStat) < 0) 
         return -2;
 
     /* Print detailed info if path is a regular file */
     if (S_ISREG(fileStat.st_mode)) {
-        /*
-        printf("Information for %s\n",path);
-        printf("---------------------------\n");
-        printf("File Size: %ld bytes \tBlock Size: %ld \tBlocks: %ld\n",fileStat.st_size, fileStat.st_blksize, fileStat.st_blocks);
-        printf("Number of Links: \t%ld\n",fileStat.st_nlink);
-        printf("File inode: \t\t%ld\n",fileStat.st_ino);
-        printf("File mode: \t\t%d\n",fileStat.st_mode);
-        printf("File Permissions: \t");
-        printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-        printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-        printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-        printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-        printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-        printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-        printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-        printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-        printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-        printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-        printf("\n\n");
-        printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
-        */
         return fileStat.st_size;
     }
     printf("Special file %s\n", strrchr(path, '/') + 1);
