113e3dc0a8b03c20d3bb66fefc30c1bbfcd687a6
Halil Onur Çeçen <cecenonur0@gmail.com>
2019-05-01 03:19:43 +0300

    - final version

--- HW6/pCp.c
+++ HW6/pCp.c
@@ -15,10 +15,13 @@
 #include <sys/stat.h>
 #include <math.h>
 
+#define FILENAME_BUFFER_SIZE 250
+
 typedef struct _entry {
     int source_fd;
     int target_fd;
-    char * filename;
+    char filename[FILENAME_BUFFER_SIZE];
+    long size;
 } Entry;
 
 typedef struct _node {
@@ -32,16 +35,30 @@ void fill_buffer(char * source_path, char * target_path);
 
 
 int producer_finished = 0;
+int consumer_finished = 0;
+int consumed_item_count = 0;
+long consumed_bytes = 0;
 int BUFFER_SIZE;
 int NUM_OF_CONSUMERS;
 int COPY_BUFFER_SIZE = 1024 * 1024;
-pthread_t *consumer_thread;
-pthread_mutex_t lock;
+pthread_t *consumer_thread, main_thread_id;
+pthread_mutex_t lock, thread_kill;
 sem_t empty, full;
 Node * head = NULL, * last;
 
 void signal_handler(int sig) {
-    pthread_exit(NULL);
+    if (main_thread_id == pthread_self()) {
+        // clear memory
+        free(consumer_thread);
+        pthread_mutex_destroy(&lock);
+        pthread_mutex_destroy(&thread_kill);
+        sem_destroy(&full);
+        sem_destroy(&empty);
+        exit(EXIT_FAILURE);
+    }
+    else {
+        pthread_exit(NULL);
+    }
 }
 
 void * producer(void *arg) {
@@ -56,11 +73,6 @@ void * producer(void *arg) {
 }
 
 void * consumer(void * arg) {
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = &signal_handler;
-    sigaction(SIGINT, &sa, NULL);
-
     int count = 0;
     if (count == 0) {
         // Give producer a chance to enter its loop
@@ -71,6 +83,10 @@ void * consumer(void * arg) {
         //printf("Consumer waiting\n");
         sem_wait(&full);
         pthread_mutex_lock(&lock);
+        if (consumer_finished) {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
         //printf("Consumer gone\n");
         Node * current = head;
         if (head != last) {     // There were more than one item in buffer
@@ -82,28 +98,37 @@ void * consumer(void * arg) {
         }
         pthread_mutex_unlock(&lock);
         sem_post(&empty);
-        printf("item = %s\n", current->entry.filename);
+        //printf("Current consumer item %s\n", current->entry.filename);
         consume(current);
         sem_getvalue(&full, &count);
     }
     // Finished consuming signal other consumer threads if theyre waiting on semaphore
-    pthread_t tid = pthread_self();
-    for(size_t i = 0; i < NUM_OF_CONSUMERS ; i++)
-    {
-        if (tid != consumer_thread[i]) {
-            pthread_kill(consumer_thread[i], SIGINT);
-        }
-    }
+    //printf("Finished consuming %ld\n", pthread_self());
+    consumer_finished = 1;
+    sem_post(&full); // Unlock other thread if they've hung in sem_wait
 
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &signal_handler;
+    sigaction(SIGINT, &sa, NULL);
+
+    main_thread_id = pthread_self();
+    
     struct timeval start, end;
-    NUM_OF_CONSUMERS = 10;
-    BUFFER_SIZE = 5;
+    NUM_OF_CONSUMERS = atoi(argv[1]);
+    BUFFER_SIZE = atoi(argv[2]);
     if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("Mutex init failed\n");
+        return 1;
+    }
+
+    if (pthread_mutex_init(&thread_kill, NULL) != 0)
     {
         printf("Mutex init failed\n");
         return 1;
@@ -121,7 +146,7 @@ int main(int argc, char *argv[])
 
     gettimeofday(&start, NULL);
     pthread_t producer_thread_id;
-    if (pthread_create(&producer_thread_id, NULL, producer, (void *) &argv[1])) {
+    if (pthread_create(&producer_thread_id, NULL, producer, (void *) &argv[3])) {
         printf("Couldn't create producer thread.\n");
         return 1;
     }
@@ -142,9 +167,12 @@ int main(int argc, char *argv[])
     free(consumer_thread);
     
     gettimeofday(&end, NULL);
+    printf("\nTotal:\n%d files\n%ld microseconds\n%ld bytes\n", consumed_item_count, (end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000, consumed_bytes);
     pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&thread_kill);
     sem_destroy(&full);
     sem_destroy(&empty);
+
     return 0;
 }
 
@@ -154,7 +182,8 @@ Entry produce(char * source_path, char * target_path, char * filename) {
     char str_buffer[size];
 
     memset(str_buffer, '\0', size);
-    entry.filename = filename;
+    memset(entry.filename, 0, FILENAME_BUFFER_SIZE);
+    strcpy(entry.filename, filename);
 
     sprintf(str_buffer, "%s/%s", source_path, filename);
     entry.source_fd = open(str_buffer, O_RDONLY);
@@ -166,8 +195,8 @@ Entry produce(char * source_path, char * target_path, char * filename) {
 }
 
 void fill_buffer(char * source_path, char * target_path) {   
-    printf("Directory to copy from: %s\n", source_path);
-    printf("Directory to copy to: %s\n", target_path);
+    // printf("Directory to copy from: %s\n", source_path);
+    // printf("Directory to copy to: %s\n", target_path);
 
     DIR * d;
     struct dirent *dir;
@@ -177,7 +206,7 @@ void fill_buffer(char * source_path, char * target_path) {
             if (strlen(dir->d_name) > 0 && dir->d_name[0] == '.') continue;
             if (dir->d_type == DT_REG) {
                 Entry entry = produce(source_path, target_path, dir->d_name);
-                
+
                 sem_wait(&empty);
                 pthread_mutex_lock(&lock);
 
@@ -214,7 +243,6 @@ void fill_buffer(char * source_path, char * target_path) {
                 else {
                     sprintf(target_buffer, "%s%s", target_path, dir->d_name);
                 }
-                printf("Inner call %s - %s\n", source_buffer, target_buffer);
                 fill_buffer(source_buffer, target_buffer);
             }
             else if (dir->d_type == DT_FIFO) {
@@ -231,8 +259,19 @@ void consume(Node * item) {
     int count = 0;
     char buffer[COPY_BUFFER_SIZE];
     memset(buffer, '\0', COPY_BUFFER_SIZE);
+    item->entry.size = 0;
+
     while((count = read(source, buffer, COPY_BUFFER_SIZE)) > 0) {
+        //printf("writing %s\n", item->entry.filename);
         write(target, buffer, count);
+        item->entry.size += count;
+        pthread_yield();
     }
+    consumed_item_count++;
+    consumed_bytes += item->entry.size;
+    close(source);
+    close(target);
+    printf("Copied %s (%ld bytes).\n", item->entry.filename, item->entry.size);
+    pthread_yield();
     free(item);
 }
